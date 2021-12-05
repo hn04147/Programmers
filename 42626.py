b73aaa4e423a3ed8b15ec9b910679a5d174e2d1e
Sangjin <hn04147@naver.com>
2021-12-05 18:48:54 +0900

    LEVEL 2 SKILL CHECK

---
+++ 42626.py
@@ -0,0 +1,17 @@
+import heapq
+
+def solution(scoville, K):
+  answer = 0
+  h = []
+
+  for i in scoville:
+    heapq.heappush(h, i)
+
+  while h[0] < K:
+    try:
+      heapq.heappush(h, heapq.heappop(h) + heapq.heappop(h) * 2)
+    except:
+      return -1
+    answer += 1
+
+  return answer
